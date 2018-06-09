b4a6eb9e00e3da46f3f78ecc0bfc6e2b3a95eafb
kjy1111 <ekfto7@nate.com>
2018-06-09 10:05:42 +0900

    Initial commit

--- problem01.py
+++ problem01.py
@@ -4,9 +4,7 @@ num = input("수를 입력하세요:")
 
 if int(num) == 0 or int(num) == 1:
     print('3의 배수가 아닙니다')
-
 elif num.isalpha():
     print('정수가 아닙니다.')
-
 elif num.isdigit():
     print('3의 배수 입니다.' if int(num) % 3 == 0 else '3의 배수가 아닙니다.')
